db8f85f9d220607d487ab2797725827e17cb97a2
James Boyle <james.patrick.boyle92@gmail.com>
2022-04-19 17:59:00 +0200

    Fix bugs + render  Carousel images

--- src/App.js
+++ src/App.js
@@ -8,26 +8,18 @@ import Recipes from "./components/Recipes";
 import Contact from "./components/Contact";
 import { Route, Routes } from "react-router-dom";
 
-
 export default function App() {
   return (
-    <><div className="App">
-      
-      <Nav />
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/recipes" element={<Recipes />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    
-  
-   
-     
-
-
- 
-    </div>
+    <>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/js/bootstrap.js";
 import "react-loading-skeleton/dist/skeleton.css";
 import "./index.css";
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "react-loading-skeleton/dist/skeleton.css";
 
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
